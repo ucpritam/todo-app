a7c91298b98bb1af7c582922df126c7cd7d4f696
Pritam Majumder <ptmmajumder630@gmail.com>
2021-10-08 15:27:22 +0530

    fixed input field and added setting

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -3,6 +3,7 @@ import 'package:todo_app/database/db_provider.dart';
 import 'package:todo_app/model/task_model.dart';
 import '../widget/todo_list.dart';
 import './add_todo.dart';
+import '../screen/setting.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -33,7 +34,20 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('To-Do App'),
+        title: const Text('To Do App'),
+        actions: [
+          IconButton(
+            onPressed: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (_) => const SettingScreen(),
+                )),
+            icon: const Icon(Icons.settings),
+          ),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 2),
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         child: const Icon(
--- lib/widget/user_input.dart
+++ lib/widget/user_input.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:todo_app/model/task_model.dart';
 
-class UserInput extends StatelessWidget {
-  var textController = TextEditingController();
+class UserInput extends StatefulWidget {
   final Function insertTask; //receive addItem method
-  UserInput({required this.insertTask, Key? key}) : super(key: key);
+  const UserInput({required this.insertTask, Key? key}) : super(key: key);
+
+  @override
+  _UserInputState createState() => _UserInputState();
+}
+
+class _UserInputState extends State<UserInput> {
+  final textController = TextEditingController();
+  bool validate = false;
 
   @override
   Widget build(BuildContext context) {
@@ -13,28 +20,37 @@ class UserInput extends StatelessWidget {
       child: Row(
         children: [
           Expanded(
-              child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 5),
-            child: TextField(
-              controller: textController,
-              decoration: const InputDecoration(
-                hintText: 'Add new task',
-                border: InputBorder.none,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: TextField(
+                controller: textController,
+                decoration: InputDecoration(
+                  hintText: 'Add new task',
+                  border: InputBorder.none,
+                  errorText: validate ? 'Enter a task' : null,
+                ),
               ),
             ),
-          )),
+          ),
           const SizedBox(
             width: 10,
           ),
-          GestureDetector(
-            onTap: () {
-              var myTodo = Task(title: textController.text, status: false);
-              insertTask(myTodo);
-              Navigator.pop(context);
+          TextButton(
+            onPressed: () {
+              setState(() {
+                textController.text.isEmpty
+                    ? validate = true
+                    : validate = false;
+              });
+              if (validate == false) {
+                var myTodo = Task(title: textController.text, status: false);
+                widget.insertTask(myTodo);
+                Navigator.pop(context);
+              }
             },
             child: Container(
               color: Colors.blue,
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 14),
               child: const Text(
                 'Add',
                 style: TextStyle(
